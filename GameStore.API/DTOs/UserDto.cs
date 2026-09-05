1bec378dbe0bbf13ef148a231d4deac48a918b9f
KramSany <friman.kop@bk.ru>
2026-09-05 19:29:52 +0300

    new: Реализована новая Dto User

---
+++ GameStore.API/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.API.DTOs;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string Password { get; set; }
+}
